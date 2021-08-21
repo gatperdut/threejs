1efb4a2086a23cf8f3266b80e727f64562e8db89
Carlos Romero <carlos.romero.breto@gmail.com>
2021-08-21 21:58:45 +0200

    add template

---
+++ threejs_template/src/index.ts
@@ -0,0 +1,46 @@
+import './styles.css';
+
+
+import { BoxGeometry, Mesh, MeshBasicMaterial, MeshBasicMaterialParameters, PerspectiveCamera, Scene, Vector3, WebGLRenderer, WebGLRendererParameters } from 'three';
+
+
+const canvas: Element = document.querySelector('.webgl');
+
+const scene: Scene = new Scene();
+
+
+const boxGeometry: BoxGeometry = new BoxGeometry(3, 4, 5);
+
+const boxMeshBasicMaterialParameters: MeshBasicMaterialParameters = {
+  color: 0xCC0000
+};
+
+const boxMeshBasicMaterial: MeshBasicMaterial = new MeshBasicMaterial(boxMeshBasicMaterialParameters);
+
+const boxMesh: Mesh = new Mesh(boxGeometry, boxMeshBasicMaterial);
+
+scene.add(boxMesh);
+
+const sizes = {
+  width: 800,
+  height: 600
+};
+
+const perspectiveCamera: PerspectiveCamera = new PerspectiveCamera(75, sizes.width / sizes.height);
+
+perspectiveCamera.translateOnAxis(new Vector3(1,1,1), 8)
+
+perspectiveCamera.lookAt(0, 0, 0);
+
+scene.add(perspectiveCamera);
+
+
+const webGLRendererParameters: WebGLRendererParameters = {
+  canvas: canvas
+};
+
+const webGLRenderer: WebGLRenderer = new WebGLRenderer(webGLRendererParameters);
+
+webGLRenderer.setSize(sizes.width, sizes.height);
+
+webGLRenderer.render(scene, perspectiveCamera);
